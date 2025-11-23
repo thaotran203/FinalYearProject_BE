24f198f1afdddcaeefb55a5f3743b5a052925a37
thaotran203 <trandieuthaoo@gmail.com>
2025-11-23 15:13:25 +0700

    implement vector embedding phase using Qdrant

---
+++ FinalYearProject_BE/FinalYearProject_BE/DTOs/EmbeddingResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FinalYearProject_BE.DTOs
+{
+    public class EmbeddingResponseDTO
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("vector")]
+        public List<float> Vector { get; set; }
+    }
+}
